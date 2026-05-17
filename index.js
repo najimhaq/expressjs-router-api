e26c3ab4145de5fa0d169b254d8fbc1ab0ac7dad
najimhaq <mdnajimulhaque@gmail.com>
2026-05-17 18:12:41 +0900

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const app = require('./app');
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`User app listening on port ${PORT}!`);
